c390732afbe63c7878d9436d2e8bae14fc89f461
Navayugan2003 <navayugan02@gmail.com>
2024-11-19 19:57:14 +0530

    created login component and add reactive form module and routing

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit{
+
+  login!:FormGroup;
+  user!:string;
+
+  constructor(private router: Router) { }
+
+
+
+  ngOnInit(): void {
+      this.login = new FormGroup({
+        userName: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+
+      });
+  }
+
+  navigateToHome() {
+    if(this.login.valid){
+      console.log(this.login.value);
+    this.router.navigate(['home']);
+    }
+  }
+
+}
